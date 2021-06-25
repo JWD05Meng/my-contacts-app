581d5372f2037fa292f10c047104efe6ab7abbcf
meng <loh.yumeng@gmail.com>
2021-06-25 16:36:03 +1000

    meh

--- test.js
+++ test.js
@@ -1,22 +1,30 @@
-const getUserChoice = userInput => {
-  userInput = userInput.toLowerCase();
-  if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
-    return userInput;
-  } else {
-    console.log('Error!');
-  };
- }
- // console.log(getUserChoice('paper'));
- // console.log(getUserChoice('fork'));
- const getComputerChoice = () => {
-   randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber) {
-    case 0:
-    return 'rock';
-    case 1:
-    return 'scissors';
-    case 2:
-    return 'paper'
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
   }
- }
- console.log(getComputerChoice());
+  get title() {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._ratings;
+  }
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
+  getAverageRating() {
+    let ratingsSum = this._ratings.reduce((currentSum, ratings) => currentSum + ratings, 0);
+    const lengthOfArray = this._ratings.length;
+    return ratingSum / lengthOfArray;
+  }
+  addRating() {
+    ratings.push();
+  }
+  set isCheckedOut(newCheckedOut) {
+    this._isCheckedOut = newCheckedOut;
+  }
+}
